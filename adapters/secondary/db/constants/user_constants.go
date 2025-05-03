7f6c759d019a0957bfe228ff045928730b2cd1a0
Lechitz <felipe.lechitz@gmail.com>
2025-05-03 11:34:05 -0300

    chore: adjust user constants

--- adapters/secondary/db/constants/user_constants.go
+++ adapters/secondary/db/constants/user_constants.go
@@ -1,25 +1,10 @@
 package constants
 
-// Error messages for user
-
-const (
-	ErrorToCreateUser        = "error to create user: %w"
-	ErrorToGetAllUsers       = "error to get all users: %w"
-	ErrorToGetUserByID       = "error to get user by ID"
-	ErrorToGetUserByUsername = "error to get user by username"
-	ErrorToUpdateUser        = "error to update user"
-	ErrorToGetUserByEmail    = "error to get user by email"
-	ErrorToSoftDeleteUser    = "error to soft delete user"
-)
-
-// Table names
-
 const (
 	TableUsers = "aion_api.users"
 )
 
 const (
-	Error     = "error"
 	CreatedAt = "created_at"
 	UpdatedAt = "updated_at"
 	DeletedAt = "deleted_at"
