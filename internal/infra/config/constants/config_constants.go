1a25de2a298e92bb791bc019dbfd3ed8f710c44b
Lechitz <felipe.lechitz@gmail.com>
2025-06-07 11:29:10 -0300

    security: suppress false positives for G101 with #nosec comments

--- internal/infra/config/constants/config_constants.go
+++ internal/infra/config/constants/config_constants.go
@@ -4,9 +4,9 @@ const (
 	Settings                  = "setting"
 	ErrFailedToProcessEnvVars = "failed to process environment variables: %v"
 	ErrGenerateSecretKey      = "failed to generate secret key"
-	SecretKeyWasNotSet        = "SECRET_KEY was not set. A new one was generated for this runtime session."
 )
 
 const (
-	SecretKeyFormat = "\nSECRET_KEY=%s\n\n"
+	SecretKeyWasNotSet = "SECRET_KEY was not set. A new one was generated for this runtime session." // #nosec G101
+	SecretKeyFormat    = "\nSECRET_KEY=%s\n\n"                                                       // #nosec G101
 )
