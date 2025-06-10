43c48def29a99750ea44a2baa1ec56e486c68b05
Lechitz <felipe.lechitz@gmail.com>
2025-06-10 13:18:44 -0300

    chore: remove unused variable

--- internal/infra/config/constants/config_constants.go
+++ internal/infra/config/constants/config_constants.go
@@ -12,6 +12,3 @@ const ErrGenerateSecretKey = "failed to generate secret key"
 
 // SecretKeyWasNotSet is logged when no SECRET_KEY is found, and a new one is generated.
 const SecretKeyWasNotSet = "SECRET_KEY was not set. A new one was generated for this runtime session." // #nosec G101
-
-// SecretKeyFormat is the format used to display the newly generated secret key.
-const SecretKeyFormat = "\nSECRET_KEY=%s\n\n" // #nosec G101
