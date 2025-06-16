7427199ed35635ae1ed08d1de8128d96529a0b4b
Lechitz <felipe.lechitz@gmail.com>
2025-06-16 01:36:17 -0300

    fix: remove unused constants

--- cmd/aion-api/constants/constants.go
+++ cmd/aion-api/constants/constants.go
@@ -49,15 +49,6 @@ const GraphqlServerStarted = "graphql server started"
 // MsgShutdownSignalReceived is a constant string logged when the application receives a shutdown signal to start graceful shutdown procedures.
 const MsgShutdownSignalReceived = "shutdown signal received, attempting graceful shutdown"
 
-// ErrHTTPGracefulShutdown is a constant string representing an error that occurs during the HTTP server's graceful shutdown process.
-const ErrHTTPGracefulShutdown = "error during graceful shutdown"
-
-// ErrGraphqlGracefulShutdown is a constant string representing an error message during the graceful shutdown of the GraphQL server.
-const ErrGraphqlGracefulShutdown = "error during graphql server graceful shutdown"
-
-// MsgGracefulShutdownSuccess indicates that the server has been successfully shut down in a graceful manner.
-const MsgGracefulShutdownSuccess = "server shutdown gracefully"
-
 // Port represents the key used to define or identify a port configuration parameter.
 const Port = "port"
 
