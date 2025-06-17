d49e845776c91b505a4f91e2c2a64a650048bff5
Lechitz <felipe.lechitz@gmail.com>
2025-06-17 12:11:30 -0300

    chore: add Category messages constants

---
+++ internal/adapters/primary/graph/constants/graph_constants.go
@@ -0,0 +1,57 @@
+package constants
+
+const UserID = "user_id"
+
+const CategoryID = "category_id"
+
+const CategoryName = "category_name"
+
+const CategoryColor = "category_color"
+
+const CategoriesCount = "categories_count"
+
+const Error = "error"
+
+const ErrUserIDNotFound = "user id not found in context"
+
+const ErrCategoryNotFound = "category not found"
+
+const InvalidCategoryID = "invalid category id"
+
+const ErrCategoryByNameNotFound = "category not found by name"
+
+const ErrAllCategoriesNotFound = "no categories found"
+
+const SuccessCategoryCreated = "category created successfully"
+
+const SuccessCategoryFetch = "category fetched successfully"
+
+const SuccessAllCategoriesFetch = "all categories fetched successfully"
+
+const SuccessCategoryUpdated = "category updated successfully"
+
+const SuccessCategorySoftDeleted = "category soft deleted successfully"
+
+const SpanTracerCategory = "GraphQL/Category"
+
+const SpanStartCreateCategory = "CreateCategory"
+
+const SpanEventCreateCategory = "start CreateCategory mutation"
+
+const SpanGetCategoryByID = "start GetCategoryByID query"
+
+const SpanGetCategoryByName = "start GetCategoryByName query"
+
+const SpanUpdateCategory = "start UpdateCategory mutation"
+
+const SpanGetAllCategories = "start GetAllCategories query"
+
+const SpanStartGetCategoryByID = "GetCategoryByID"
+
+const SpanStartAllGetCategories = "GetAllCategories"
+
+const SpanStartGetCategoryByName = "GetCategoryByName"
+
+const SpanStartUpdateCategory = "UpdateCategory"
+
+const SpanStartSoftDeleteCategory = "SoftDeleteCategory"
