c1e0889040b087caf31288c6224775c0cbd33685
Lechitz <felipe.lechitz@gmail.com>
2025-07-05 16:30:51 -0300

    chore: adjust mocks path

--- makefiles/codegen.mk
+++ makefiles/codegen.mk
@@ -11,32 +11,27 @@ mocks:
 	@echo "Generating mocks for output ports and usecases..."
 	@mkdir -p tests/mocks/token tests/mocks/user tests/mocks/security tests/mocks/logger tests/mocks/category
 	@echo "→ TokenStore"
-	mockgen -source=internal/core/ports/output/cache/token_output.go \
+	mockgen -source=internal/core/ports/output/token_output.go \
 	  -destination=tests/mocks/token/mock_token_store.go \
 	  -package=tokenmocks \
 	  -mock_names=Store=MockTokenStore
-	@echo "→ TokenUsecase"
-	mockgen -source=internal/core/usecase/token/token_usecase.go \
-	  -destination=tests/mocks/token/mock_token_usecase.go \
-	  -package=tokenmocks \
-	  -mock_names=Usecase=MockTokenUsecase
 	@echo "→ UserStore"
-	mockgen -source=internal/core/ports/output/db/user_output.go \
+	mockgen -source=internal/core/ports/output/user_output.go \
 	  -destination=tests/mocks/user/mock_user_store.go \
 	  -package=usermocks \
 	  -mock_names=UserStore=MockUserStore
 	@echo "→ CategoryStore"
-	mockgen -source=internal/core/ports/output/db/category_output.go \
+	mockgen -source=internal/core/ports/output/category_output.go \
 	  -destination=tests/mocks/category/mock_category_store.go \
 	  -package=categorymocks \
 	  -mock_names=CategoryStore=MockCategoryStore
 	@echo "→ SecurityStore"
-	mockgen -source=internal/core/ports/output/security/hasher_output.go \
+	mockgen -source=internal/core/ports/output/hasher_output.go \
 	  -destination=tests/mocks/security/mock_security_store.go \
 	  -package=securitymocks \
 	  -mock_names=Store=MockSecurityStore
 	@echo "→ Logger"
-	mockgen -source=internal/core/ports/output/logger/logger_output.go \
+	mockgen -source=internal/core/ports/output/logger_output.go \
 	  -destination=tests/mocks/logger/mock_logger.go \
 	  -package=loggermocks \
 	  -mock_names=Logger=MockLogger
