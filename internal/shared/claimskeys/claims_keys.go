b24b7f94cd7902459482c04613189938e1a71f86
Lechitz <felipe.lechitz@gmail.com>
2025-07-07 23:58:38 -0300

    feature: add contextkeys, common, and claimkeys packages

---
+++ internal/shared/claimskeys/claims_keys.go
@@ -0,0 +1,8 @@
+// Package claimskeys contains constants used for JWT claims.
+package claimskeys
+
+// UserID is the key to identify the user ID in JWT claims.
+const UserID = "user_id"
+
+// Exp is the key to identify the expiration time in JWT claims.
+const Exp = "exp"
