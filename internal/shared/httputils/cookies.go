cef8ab5ac5aa38d586b6181fa718be3fbc233133
Lechitz <felipe.lechitz@gmail.com>
2025-07-09 01:23:15 -0300

    feature: add mapSameSite function

--- internal/shared/httputils/cookies.go
+++ internal/shared/httputils/cookies.go
@@ -5,33 +5,47 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/lechitz/AionApi/internal/adapters/primary/http/constants"
+	"github.com/lechitz/AionApi/internal/platform/config"
+	"github.com/lechitz/AionApi/internal/shared/commonkeys"
 )
 
 // SetAuthCookie sets a secure HTTP-only authentication cookie with the given token and expiration configuration.
-func SetAuthCookie(w http.ResponseWriter, token string, maxAge int) {
+func SetAuthCookie(w http.ResponseWriter, token string, cfg config.CookieConfig) {
 	http.SetCookie(w, &http.Cookie{
-		Name:     constants.AuthToken,
+		Name:     commonkeys.AuthToken,
 		Value:    token,
-		Path:     constants.Path,
-		Domain:   constants.Domain,
+		Path:     cfg.Path,
+		Domain:   cfg.Domain,
 		HttpOnly: true,
 		Secure:   true,
-		SameSite: http.SameSiteStrictMode,
-		MaxAge:   maxAge,
+		SameSite: mapSameSite(cfg.SameSite),
+		MaxAge:   cfg.MaxAge,
 	})
 }
 
 // ClearAuthCookie invalidates the authentication cookie by setting its value to empty and expiration to a past timestamp.
-func ClearAuthCookie(w http.ResponseWriter) {
+func ClearAuthCookie(w http.ResponseWriter, cfg config.CookieConfig) {
 	http.SetCookie(w, &http.Cookie{
-		Name:     constants.AuthToken,
+		Name:     commonkeys.AuthToken,
 		Value:    "",
-		Path:     constants.Path,
+		Path:     cfg.Path,
 		MaxAge:   -1,
 		Expires:  time.Unix(0, 0),
 		HttpOnly: true,
 		Secure:   true,
 		SameSite: http.SameSiteStrictMode,
 	})
+}
+
+func mapSameSite(sameSite string) http.SameSite {
+	switch sameSite {
+	case "Strict":
+		return http.SameSiteStrictMode
+	case "Lax":
+		return http.SameSiteLaxMode
+	case "None":
+		return http.SameSiteNoneMode
+	default:
+		return http.SameSiteDefaultMode
+	}
 }
