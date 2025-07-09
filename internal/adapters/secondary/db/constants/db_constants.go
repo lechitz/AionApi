d6fe5b04429de3da4551e3d280fc36fdd9322923
Lechitz <felipe.lechitz@gmail.com>
2025-07-09 01:23:41 -0300

    feature: adjust token existence validation in CreateToken

---
+++ internal/adapters/secondary/db/constants/db_constants.go
@@ -0,0 +1,10 @@
+// Package constants contains constants used for database operations.
+package constants
+
+const (
+	// TableUsers is the name of the database table for users.
+	TableUsers = "aion_api.users"
+
+	// CategoryTable is the name of the database table for tag categories.
+	CategoryTable = "aion_api.tag_categories"
+)
