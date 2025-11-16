02ef9f41669fc4a3230da1e9fdefa08f33dc062e
Lechitz <felipe.lechitz@gmail.com>
2025-11-16 15:38:40 -0300

    refactor: adjust user seeds

--- infrastructure/db/seed/user.sql
+++ infrastructure/db/seed/user.sql
@@ -4,4 +4,5 @@ VALUES
     ('Bruno Amaral','bruno','$2a$10$AQjsEvApPM59JVg4XIqEluc.JtKfnyIWPLK7QEYW6CPDN9cU/OH7O','bruno@aionapi.dev','user'),
     ('Carla Fernandes','carla','$2a$10$AQjsEvApPM59JVg4XIqEluc.JtKfnyIWPLK7QEYW6CPDN9cU/OH7O','carla@aionapi.dev','user'),
     ('Daniel Dias','daniel','$2a$10$AQjsEvApPM59JVg4XIqEluc.JtKfnyIWPLK7QEYW6CPDN9cU/OH7O','daniel@aionapi.dev','user'),
-    ('Ester Rodrigues','ester','$2a$10$AQjsEvApPM59JVg4XIqEluc.JtKfnyIWPLK7QEYW6CPDN9cU/OH7O','ester@aionapi.dev','user');
+    ('Ester Rodrigues','ester','$2a$10$AQjsEvApPM59JVg4XIqEluc.JtKfnyIWPLK7QEYW6CPDN9cU/OH7O','ester@aionapi.dev','user')
+ON CONFLICT (username) DO NOTHING;
